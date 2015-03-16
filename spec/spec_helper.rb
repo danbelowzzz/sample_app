cfe15761cf8307c77abcb50df5142eff27c58430
Dan Bely <kyrolesov@bk.ru>
2015-03-17 00:31:05 +0300

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -175,3 +175,10 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
